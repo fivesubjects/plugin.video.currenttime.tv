4cdef32d3da0484e8364740fc75edbf73cab84ae
dicoka <dicoka@ukr.net>
2016-11-23 19:57:05 +0300

    1. addVideoDir function def-ed for playable video dir item generation    with "&quot;" correction in title and plot 2. new item "Last Videos" in initial menu

--- addon.py
+++ addon.py
@@ -7,7 +7,10 @@ import urllib2, re
 main_menu = ([['play', 'Прямой эфир', 'live', 'http://rfe-lh.akamaihd.net/i/rfe_tvmc5@383630/master.m3u8',
                'Круглосуточный телеканал Настоящее Время - прямая трасляция онлайн'
               ],
-              ['last_vids_fldr', 'Час Тимура Олевского', 'olevski', '/z/20333.html',
+              ['last_vids_fldr', 'Последние эфиры', 'lastall', '/z/17317.html',
+               'Эфиры телепередач за последние несколько деней.'
+               ],
+               ['last_vids_fldr', 'Час Тимура Олевского', 'olevski', '/z/20333.html',
                'Ежедневная телепередача \"Час Тимура Олевского\".\n'
                'Час Тимура Олевского – это самые интересные и важные события, за которыми следит команда наших журналистов. Мы не бежим за новостями, а находим их для вас. Мы показываем все точки зрения и рассказываем обо всем, что случилось в конце рабочего дня. С понедельника по пятницу.\n'
               ],
@@ -43,11 +46,15 @@ base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 
-xbmcplugin.setContent(addon_handle, 'tvshows')
-xbmcplugin.setPluginCategory(int(sys.argv[1]), 'tv')
+mode = args.get('mode', None)
+furl = args.get('folderurl', None)
+ftitle = args.get('title', None)
 
 site_url = 'http://www.currenttime.tv'
 ptv = xbmcaddon.Addon('plugin.video.currenttime.tv')
+
+xbmcplugin.setContent(addon_handle, 'tvshows')
+xbmcplugin.setPluginCategory(int(sys.argv[1]), 'tv')
 
 
 def build_url(query):
@@ -76,6 +83,25 @@ def addDir(arg):
     ok = xbmcplugin.addDirectoryItem(handle=addon_handle, url=arg['url'], listitem=li, isFolder=isFolder)
     return ok
 
+def addVideoDir(url):
+    page = readPage(site_url + url)
+    match1 = re.compile('<a class="html5PlayerImage" href="(.+?)">\n'
+                        '<img src="(.+?)"').findall(page)
+    match_title = re.compile('<meta name="title" content="(.+?)" />').findall(page)
+    match_plot = re.compile('<meta name="description" content="(.+?)" />', re.DOTALL).findall(page)
+    if len(match_plot) < 1: match_plot = [' ']
+    arg = {
+        'thumb': re.sub(r'_w\w+', '_w512_r1.jpg', match1[0][1]),
+        'fanart': re.sub(r'_w\w+', '_w1920_r1.jpg', match1[0][1]),
+        'mode': 'play',
+        'title': re.sub('&quot;', '"', match_title[0]),
+        'plot': re.sub('&quot;', '"', match_plot[0]),
+        'url': match1[0][0]
+
+    }
+    addDir(arg)
+
+
 def readPage(url):
     req = urllib2.Request(url)
     req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
@@ -84,9 +110,7 @@ def readPage(url):
     response.close()
     return page
 
-mode = args.get('mode', None)
-furl = args.get('folderurl', None)
-ftitle = args.get('title', None)
+
 
 if mode is None:
     for mode, title, name, url, plot in main_menu:
@@ -103,36 +127,21 @@ if mode is None:
 
 elif mode[0] == 'last_vids_fldr':
     page = readPage(site_url + furl[0])
-    match1 = re.compile('<div class="thumb listThumb thumb16_9">\n'
-                       '<img data-src="" src="(.+?_tv.+?jpg)" alt="(.+?)">\n'
-                       '</div>\n<i class="ico ico-video"></i>\n'
-                       '</a>\n<div class="content">\n'
-                       '<span class="date" >(.+?)</span>\n'
-                       '<a href="(.+?)" >\n<h4>\n'
-                       '<span class="title">(.+?)</span>\n'
-                       '</h4>\n<p>(.+?)</p>').findall(page)
-    for img, altname, date, url, title, plot in match1:
-        page = readPage(site_url + url)
-        match2 = re.compile('<a class="html5PlayerImage" href="(.+?)"').findall(page)
-        arg = {
-            'thumb': re.sub(r'_w\w+', '_w512_r1.jpg', img),
-            'fanart': re.sub(r'_w\w+', '_w1920_r1.jpg', img),
-            'mode': 'video',
-            'title': title,
-            'plot': plot,
-            'url': match2[0]
-        }
-        addDir(arg)
+    match = re.compile('width-img size-[2|3]">\n'
+                       '<a href="(.+?)"\n'
+                       'class="img-wrapper"').findall(page)
+    for url in match:
+        addVideoDir(url)
 
-    arg = {
-        'thumb': img_link('live', 'thumb'),
-        'fanart': img_link('live', 'fanart'),
-        'mode': 'all_vids_fldr',
-        'title': '>>>... Архив. '+ ftitle[0],
-        'plot': 'Видео за все даты',
-        'url': furl[0]
-    }
-    addDir(arg)
+    if ftitle[0] != 'Последние эфиры':
+        arg = {'thumb': img_link('live', 'thumb'),
+               'fanart': img_link('live', 'fanart'),
+               'mode': 'all_vids_fldr',
+               'title': '>>>... Архив. '+ ftitle[0],
+               'plot': 'Видео за все даты',
+               'url': furl[0]
+              }
+        addDir(arg)
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'all_vids_fldr':
@@ -153,19 +162,7 @@ elif mode[0] == 'all_vids_fldr':
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'video':
-    page = readPage(site_url + furl[0])
-    match1 = re.compile('<a class="html5PlayerImage" href="(.+?)">\n'
-                       '<img src="(.+?)"').findall(page)
-    match2 = re.compile('<meta name="title" content="(.+?)" />\n'
-                        '<meta name="description" content="(.+?)"').findall(page)
-    arg = {
-            'thumb': re.sub(r'_w\w+', '_w512_r1.jpg', match1[0][1]),
-            'fanart': re.sub(r'_w\w+', '_w1920_r1.jpg', match1[0][1]),
-            'mode': 'play',
-            'title': match2[0][0],
-            'plot': match2[0][1],
-            'url': match1[0][0]
-        }
-    addDir(arg)
+    addVideoDir(furl[0])
+
     xbmcplugin.endOfDirectory(addon_handle)
 
